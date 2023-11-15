4e0b170577793214af8235a4842e7c4cc8df9f7f
aaapark <parksc1534.dev@gmail.com>
2023-11-15 17:45:06 +0900

    feat: 인사말 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,10 +1,8 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const OutputView = {
-    printMenu() {
-        MissionUtils.Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
+    printGreeting() {
+        MissionUtils.Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    },
 }
 export default OutputView;
