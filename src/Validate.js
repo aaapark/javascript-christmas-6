2320daba1ebcb27ad892cd8c83d3101aef7ed140
aaapark <parksc1534.dev@gmail.com>
2023-11-15 18:06:16 +0900

    feat: 주문 메뉴 유효성 검사 작성

--- src/Validate.js
+++ src/Validate.js
@@ -1,3 +1,5 @@
+import {MENU_LIST, ALL_MENU_LIST} from "./const.js"
+
 const Validation = {
     //입력 날짜 확인
     checkDate(date) {
@@ -21,7 +23,63 @@ const Validation = {
     },
 
     checkUserMenuList(menuList) {
-        console.log(menuList);
+        const nameList = menuList.map((menu) => menu.name)
+        const amountList = menuList.map((menu) => menu.amount)
+        let inputAmountBeNumber = amountList.map(Number)
+        this.checkOrderMenuIsValid(nameList);
+        this.checkOrderManuDuplication(nameList);
+        this.checkOrderAmountIsVliad(inputAmountBeNumber);
+        this.checkOrderAmountIsInteger(inputAmountBeNumber);
+        this.checkTotalOrderAmount(inputAmountBeNumber);
+        this.checkAllOrderAreBeverage(nameList);    
+    },
+
+    checkOrderMenuIsValid(input) { 
+        const allMenuList = Object.keys(ALL_MENU_LIST)
+        if(input.some((i) => !allMenuList.includes(i))) {
+            throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
+        }
+        return true;
+    },
+
+    checkOrderManuDuplication(input) {
+        let set = new Set(input)
+        if(set.size != input.length) {
+            throw new Error ('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
+        }
+        return true;
+    },
+
+    checkAllOrderAreBeverage(input) {
+        const menuBeverageList = Object.keys(MENU_LIST.MENU_BEVERAGE)
+        if(input.every((i) => menuBeverageList.includes(i))) {
+            throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
+        }
+        return true;
+    },
+
+    checkOrderAmountIsInteger(input) {
+        if(input.some((i) => !Number.isInteger(i))) {
+            throw new Error ('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요..')
+        }
+        return true;
+    },
+
+    checkOrderAmountIsVliad(input) {
+        if(input.some((i) => i <= 0)) {
+            throw new Error ('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.')
+        }
+        return true;
+    },
+
+    checkTotalOrderAmount(input) {
+        const totalAmount = input.reduce((sum,currVal) => {
+            return sum + currVal
+        }, 0)
+        if (totalAmount >= 21) {
+            throw new Error ('[ERROR] 최대 20개까지만 주문 가능합니다.')
+        }
+        return true;
     },
 };
 export default Validation;
