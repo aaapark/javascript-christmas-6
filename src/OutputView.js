4767b9e23fc04baec976ed89d9610145c400ec43
aaapark <parksc1534.dev@gmail.com>
2023-11-15 17:20:52 +0900

    fix: InputView.js, OutputView.js export 부분 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,10 @@
-export default OutputView = {
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const OutputView = {
     printMenu() {
-        Console.print("<주문 메뉴>");
+        MissionUtils.Console.print("<주문 메뉴>");
         // ...
     }
     // ...
 }
+export default OutputView;
